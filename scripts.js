8956b30d14ead1467b6194e67f4842c4650e002d
CandersA <123170339+CandersA@users.noreply.github.com>
2023-01-30 14:45:09 +0200

    Add range input for amount of pixels in a row

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ console.log("JS");
 
 const sketchPad = document.querySelector(".sketch-pad");
 let padSize = sketchPad.offsetHeight;
+let pixelRow = 16;
+createGrid(padSize, pixelRow);
 
 function createGrid(padSize, pixelRow) {
     let pixelSize = padSize / pixelRow;
     let pixelAmount = pixelRow * pixelRow;
-    
+
     i = 0;
     while (i < pixelAmount) {
         let onePixel = document.createElement("div");
@@ -18,4 +20,13 @@ function createGrid(padSize, pixelRow) {
     }
 }
 
-createGrid(padSize, 29);
+let slider = document.getElementById("myRange");
+const rangeDisplay = document.getElementById("rangedisplay");
+
+slider.oninput = function() {
+    rangeDisplay.textContent = this.value;
+    sketchPad.textContent = '';
+    pixelRow = this.value;
+    console.log(pixelRow);
+    createGrid(padSize, pixelRow);
+  }
