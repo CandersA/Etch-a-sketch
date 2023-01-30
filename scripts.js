011fdf84d2d798137683ea8dba7599d5a06a4012
CandersA <123170339+CandersA@users.noreply.github.com>
2023-01-30 14:13:04 +0200

    Add logic to create sketch pad grid

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 console.log("JS");
+
+const sketchPad = document.querySelector(".sketch-pad");
+let padSize = sketchPad.offsetHeight;
+
+function createGrid(padSize, pixelRow) {
+    let pixelSize = padSize / pixelRow;
+    let pixelAmount = pixelRow * pixelRow;
+    
+    i = 0;
+    while (i < pixelAmount) {
+        let onePixel = document.createElement("div");
+        onePixel.classList.add("pixel");
+        onePixel.style.height = `${pixelSize}px`;
+        onePixel.style.width = `${pixelSize}px`;
+        sketchPad.appendChild(onePixel);
+        i++;
+    }
+}
+
+createGrid(padSize, 29);
